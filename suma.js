e9ebe4cd9e2a6bd30ce09829e0fe05419d2c34db
Alex Yazmin Manzo Olguin <ada_hack418@hackademy.mx>
2022-03-19 13:09:14 -0600

    ejercicio resuelto

---
+++ suma.js
@@ -0,0 +1,9 @@
+console.log("Manzo Olguin Alex Yazmin")
+let x;
+let y;
+x=prompt('Ingrese primer numero:');
+y=prompt('ingrese el segundo numero:');
+let suma= parseFloat(x)+ parseFloat(y);
+document.write('la suma es:');
+document.write(x,'+',y,'=');
+document.write(suma);
