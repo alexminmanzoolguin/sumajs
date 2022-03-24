95e180df0b4b3c692b4fe5edb5b89661a7adf6b1
Alex Yazmin Manzo Olguin <ada_hack418@hackademy.mx>
2022-03-23 22:37:10 -0600

    poner estilos y readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sumajs
+# Sumajs
  Valor que regresa La suma de los números enteros de arriba
+## soluccion 
+> Buscar la forma de diseñar una pagina donde pueda hace la funcion
+
+### prueba que funciona
+![imagenuno](./prueba.png)
--- suma.js
+++ suma.js
@@ -1,9 +1,8 @@
 console.log("Manzo Olguin Alex Yazmin")
-let x;
-let y;
-x=prompt('Ingrese primer numero:');
-y=prompt('ingrese el segundo numero:');
-let suma= parseFloat(x)+ parseFloat(y);
-document.write('la suma es:');
-document.write(x,'+',y,'=');
-document.write(suma);
+function sumas(){
+    var x = document.getElementById("x").value;
+    var y = document.getElementById("y").value;
+    
+    var resultado= (parseFloat(x)+ parseFloat(y));
+    alert("el resultado es:"+ resultado);
+}
